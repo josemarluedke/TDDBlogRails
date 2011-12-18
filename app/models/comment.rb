9322cbd475ebdb421b20e21124ddb5b34a2960f4
Josemar Davi Luedke <josemarluedke@gmail.com>
2011-12-18 21:11:20 -0200

    Fixed bug in associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,12 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
-  validates :name, :email, :body, :post_id, :presence => true
+  validates_associated :post
+  validates :name, :email, :body, :post, :presence => true
   validates :name, :email, :length => {:maximum => 100}
   validates_numericality_of :post_id, :only_integer => true
   validates :email, :email => true
   validates_url_format_of :url, :allow_nill => true
-  validates_associated :post
+  
   validate do 
     if post
       # Post should be available for comments
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
+  validates_associated :user
   
-  validates :title, :body, :user_id, :presence => true
+  validates :title, :body, :user, :presence => true
   validates_numericality_of :user_id, :only_integer => true
   validates :title, :length => {:maximum => 250}
   validates :excerpt, :length => {:maximum => 250}, :presence => true
-  validates_associated :user
 end
